a66e9a4158ceac5f58e27f6007027c1bb66108a3
gdtoodou <butterknife@163.com>
2017-08-28 13:44:01 +0800

    first conmmit

---
+++ app/src/main/java/cn/book/kotlindemo/BaseActivity.kt
@@ -0,0 +1,17 @@
+package cn.book.kotlindemo
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * 作者：HANCL on 2017/6/27
+ * 邮箱：butterknife@163.com
+ * 说明：
+ */
+class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
